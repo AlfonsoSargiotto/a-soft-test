6eb89c92963c0727858a9c05270e5530212615df
Alfonso Sargiotto <a.sargiotto@itecriocuarto.org.ar>
2021-05-21 18:50:29 -0300

    refactoring CLI script

--- seatmap_parser.py
+++ seatmap_parser.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--filename", type=str,
                     help="Name of the file. Must be in 'data' folder")
 parser.add_argument("-v", "--verbosity",
-                    help="E.g: python3 seatmap_parser.py seatmap1.xml ", action="store_true")
+                    help="E.g: python3 seatmap_parser.py --filename seatmap1.xml ", action="store_true")
 
 args = parser.parse_args()
 
